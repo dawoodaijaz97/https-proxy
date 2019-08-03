0104accbea71ab71d4c32eb1c0666728f7b4b62f
Dawood <dawood.aijaz97@gmail.com>
2019-08-04 02:33:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const http = require("http")
+const httpproxy = require("http-proxy")
+app = express()
+
+var cert = fs.readFileSync(__dirname + '/certsFiles/NewCert.pfx');
+var cert2 = fs.readFileSync(__dirname + '/certsFiles/selfsigned.crt', "utf-8");
+var key = fs.readFileSync(__dirname + '/certsFiles/selfsigned.key', "utf-8")
+
+
+var credentials = {
+    pfx: cert,
+    passphrase: "google"
+};
+
+//GET home route
+proxy_server = httpproxy.createServer({
+    target: {
+        host: "localhost",
+        port: 9009
+    },
+    secure: true,
+    ssl: {
+        key: key,
+        cert: cert2
+    }
+}).listen(8009, function() {
+    console.log("Proxy Running")
+})
+
+app.use(function(req, res) {
+
+    console.log(JSON.stringify(req.headers.host))
+    host = req.hostname
+    console.log(host)
+    try {
+        proxy_server.web(req, res, {
+            target: "https://" + req.hostname
+        });
+    } catch (e) {
+        console.error(e)
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World.');
+});
+
+var http_server = http.createServer(app);
+
+
+http_server.listen(9001, () => {
+    console.log("Https server listing on port : 3001")
+});
