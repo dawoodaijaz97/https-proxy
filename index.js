27370e2be2b0038d9b75c82485b08e9dc36f21e3
Dawood <dawood.aijaz97@gmail.com>
2019-08-10 20:15:32 +0500

    #

--- index.js
+++ index.js
@@ -26,19 +26,18 @@ proxy_server = httpproxy.createServer({
     console.log("Proxy Running : 443")
 })
 
-app.use(function(req, res, next) {
+app.use(function(req, res) {
 
     console.log(JSON.stringify(req.headers.host))
-        // host = req.hostname
-        // console.log(host)
-        // try {
-        //     proxy_server.web(req, res, {
-        //         target: "https://" + req.hostname
-        //     });
-        // } catch (e) {
-        //     console.error(e)
-        // }
-    next();
+    host = req.hostname
+    console.log(host)
+    try {
+        proxy_server.web(req, res, {
+            target: "https://" + req.hostname
+        });
+    } catch (e) {
+        console.error(e)
+    }
 });
 
 app.get('/', (req, res) => {
